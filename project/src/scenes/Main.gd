0c0a72de1dd3a241daf756d5458c82d7f9bfdc60
Rafael <jiyuunotameni@hotmail.com>
2024-01-04 19:48:37 -0300

    update main

--- project/src/scenes/Main.gd
+++ project/src/scenes/Main.gd
@@ -1,22 +1,17 @@
 extends Node
-@export var deskLayer: Node
-@export var desk: Control
-@export var questionLabel: RichTextLabel
-@export var endButton: TextureButton
-@export var responseTextEdit: TextEdit
-@export var animationPlayer: AnimationPlayer
 @export var effectAnimationPlayer: AnimationPlayer
 
 func _init():
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 
 func _ready():
-	Dialogic.start('cap2-part2')
+	Dialogic.start('cap4-part2-game')
 	get_viewport().set_input_as_handled()
 
 func _on_dialogic_signal(argument:String):
 	if argument == 'close':
 		get_tree().quit()
+		return
 
 	if effectAnimationPlayer.has_animation(argument):
 		effectAnimationPlayer.play(argument)
