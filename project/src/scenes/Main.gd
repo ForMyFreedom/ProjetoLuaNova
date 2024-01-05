b9281e0f5318bc9a288cd07cd4efbf29397f25ed
Rafael <jiyuunotameni@hotmail.com>
2024-01-05 09:26:34 -0300

    cap5 revision

--- project/src/scenes/Main.gd
+++ project/src/scenes/Main.gd
@@ -5,7 +5,7 @@ func _init():
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 
 func _ready():
-	Dialogic.start('cap4-part2-game')
+	Dialogic.start('cap5-part3')
 	get_viewport().set_input_as_handled()
 
 func _on_dialogic_signal(argument:String):
