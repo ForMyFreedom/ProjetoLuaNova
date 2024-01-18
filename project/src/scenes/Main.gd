1365829b3e2abc5f0010c2917788b71cea473380
Rafael <jiyuunotameni@hotmail.com>
2024-01-18 10:22:17 -0300

    a

--- project/src/scenes/Main.gd
+++ project/src/scenes/Main.gd
@@ -5,7 +5,7 @@ func _init():
 	Dialogic.signal_event.connect(_on_dialogic_signal)
 
 func _ready():
-	Dialogic.start('cap1-part1')
+	Dialogic.start('start')
 	get_viewport().set_input_as_handled()
 
 func _on_dialogic_signal(argument:String):
